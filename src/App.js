5453683e1b20ece82fdbb793faa60711730ee9b7
Aqib Iqbal <Mailaqibiqbal@gmail.com>
2022-01-02 05:28:46 +0530

    ecart3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import logo from './logo.svg';
 import Header from './components/layout/Header';
 import Sidenav from './components/layout/Sidenav';
+import Slider from './components/layout/Slider';
+import Topdeal from './components/layout/Topdeal';
 import './App.css';
 
 function App() {
   return (
     <>
-   <Header />
-   <div className="row mx-0">
-      <div className="col-lg-2">
-      <Sidenav />
+      <Header />
+
+      <div className="row mx-0 border p-3">
+
+        <div className="col-lg-2">
+          <Sidenav />
+        </div>
+
+        <div className="col-lg-10">
+          <Slider />
+        </div>
       </div>
-   </div>
-   </>
+
+      <Topdeal />
+
+    </>
   );
 }
 
